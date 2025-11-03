49c728dc66f9ba51bb4a441d85232d82d6489ad6
munjin0201 <munjin0201@fasoo.com>
2025-11-03 12:40:26 +0900

    Initial commit

---
+++ src/test/java/com/webflux_toy/WebfluxToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux_toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
